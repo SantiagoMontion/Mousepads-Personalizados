e108dc0e55d25bd1e812a52d25203d5913faeb48
santiago montion <santiagomontion@gmail.com>
2023-08-02 22:08:45 -0300

    Target not blank

--- src/EmailLink.js
+++ src/EmailLink.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./MousepadContainer.css";
 const EmailLink = ({ emailAddress, subject, body }) => {
-  const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}&taget="_blank"`;
+  const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(
+    subject
+  )}&body=${encodeURIComponent(body)}&taget="_blank"`;
 
   return (
-    <a className="button-email" href={mailtoLink} target="_blank">
+    <a className="button-email" href={mailtoLink}>
       Enviar Diseño
     </a>
   );

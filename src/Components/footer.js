cee82265b287c1e3241aeb3df1d5a87d1617e0d6
santiago montion <santiagomontion@gmail.com>
2024-02-08 13:20:56 -0300

    Links bugs fix + instagram bug

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -9,24 +9,39 @@ const Footer = () => {
     <div className="footer-container">
       <img src={Logo}></img>
       <div className="footer-links">
-        <a href="https://www.mgmgamers.store/" target="_blank" className="footer-a">
+        <a
+          href="https://mgmgamers.store/mousepads/personalizados/"
+          target="_blank"
+          className="footer-a"
+        >
           <h3 className="footer-name">Personalizados</h3>
         </a>
-        <a href="https://www.mgmgamers.store/" target="_blank" className="footer-a">
+        <a
+          href="https://www.mgmgamers.store/"
+          target="_blank"
+          className="footer-a"
+        >
           <h3 className="footer-name">Website Oficial</h3>
         </a>
-        <a href="https://www.mgmgamers.store/" target="_blank" className="footer-a">
+        <a
+          href="https://mgmgamers.store/contacto/"
+          target="_blank"
+          className="footer-a"
+        >
           <h3 className="footer-name">Contacto</h3>
         </a>
       </div>
       <div className="footer-link-logos">
-        <a href="https://www.mgmgamers.store/" target="_blank">
+        <a href="https://wa.link/1iphh6" target="_blank">
           <img className="footer-logo" src={wsp}></img>
         </a>
-        <a href="https://www.mgmgamers.store/" target="_blank">
+        <a href="https://www.instagram.com/mgmgamers.store" target="_blank">
           <img className="footer-logo" src={insta}></img>
         </a>
-        <a href="https://www.mgmgamers.store/" target="_blank">
+        <a
+          href="https://www.tiktok.com/@mgmgamers?_t=8jiR0EVmARa&_r=1"
+          target="_blank"
+        >
           <img className="footer-logo" src={tiktok}></img>
         </a>
       </div>
